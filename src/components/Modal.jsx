7832d64fcae9a8837d8c7a026fcfdf0bc5c14a4d
Samir Rodas <serodas9710@gmail.com>
2023-04-27 18:30:32 -0500

    Remove comments

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,23 +9,6 @@ const Modal = ({ children }) => {
         children,
         document.getElementById('modal')
     );
-    // const { state, addState } = useContext(AppContext);
-    // const images = state.images;
-    // const description = state.description;
-    // const modal = state.showModal ? 'modal' : 'modal hidden';
-
-    // const handleClick = () => {
-    //     addState({
-    //         ...state,
-    //         images: [],
-    //         description: '',
-    //         showModal: false
-    //     });
-    // };
-
-    // return (
-
-    // );
 }
 
 export default Modal;
